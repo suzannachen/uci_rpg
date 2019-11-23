f9c82adb35056ac74fd1b8541abfbeb20a945cfd
Unknown <suzannc1@uci.edu>
2019-11-23 13:05:06 -0800

    Main Menu + Transitions

---
+++ UCI RPG/Assets/Scripts/AnomalyStudiosVideo.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+using UnityEngine.SceneManagement;
+
+public class AnomalyStudiosVideo : MonoBehaviour
+{
+    double currentTime;
+    [SerializeField] double videoTime = .4f;
+    VideoPlayer video;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        video = GetComponent<VideoPlayer>();
+        videoTime = video.clip.length;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentTime += Time.deltaTime;
+        if(!(currentTime < videoTime))
+        {
+        	Debug.Log("Ending");
+            SceneManager.LoadScene("Main Menu");
+        }
+        
+    }
+}
---
+++ UCI RPG/Assets/Scripts/LevelChanger.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelChanger : MonoBehaviour
+{
+	public Animator animator;
+
+	private int levelToLoad;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+        	FadeToNextLevel();
+        }
+    }
+
+    public void FadeToNextLevel()
+    {
+    	FadeToLevel(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void FadeToLevel(int levelIndex)
+    {
+    	levelToLoad = levelIndex;
+    	animator.SetTrigger("Fade Out");
+    }
+
+    public void OnFadeComplete()
+    {
+    	SceneManager.LoadScene(levelToLoad);
+    }
+}
