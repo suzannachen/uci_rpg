7b5e5beb50ff9877a06d16a9f89f2e95c0bb8995
Benjamin Ponce <34080678+benponce123@users.noreply.github.com>
2019-11-24 16:09:02 -0800

    Add files via upload

--- UCI RPG/Assets/Scripts/PlayerMovement.cs
+++ UCI RPG/Assets/Scripts/PlayerMovement.cs
@@ -10,39 +10,61 @@ public class PlayerMovement : MonoBehaviour
     //public float runSpeed = 40f;
 
     //float horizontalMove = 0f;
-    int speed = 4;
-
-    //float verticalMove = 0f;
+    
+    public Animator animator;
+    public Vector2 movementDirection;
+    public float speed;
 
 
     // Update is called once per frame
     void Update()
     {
-        // horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
-        //verticalMove = Input.GetAxisRaw("Vertical") * runSpeed;
-        //Debug.Log(Input.GetAxisRaw("Vertical"));
-        
+        ProcessInputs();
+        Move();
+        Animate();
+    }
+
+    void ProcessInputs()
+    {
+        movementDirection = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        speed = Mathf.Clamp(movementDirection.magnitude, 0.0f, 5.0f);
+        movementDirection.Normalize();
+    }
+
+    void Move()
+    {
         var hor = Input.GetAxis("Horizontal");
         var ver = Input.GetAxis("Vertical");
-        //transform.Translate(hor * speed * Time.deltaTime, ver * speed * Time.deltaTime, 0);
 
+        
+        //transform.Translate(hor * speed * Time.deltaTime, ver * speed * Time.deltaTime, 0);
         //Debug.Log(hor);
 
         if (hor != 0)
         {
-            transform.Translate(hor * speed * Time.deltaTime, 0, 0);
+            transform.Translate(hor * speed * Time.deltaTime * 4, 0, 0);
         }
 
         else if (ver != 0)
         {
-            transform.Translate(0, ver * speed * Time.deltaTime, 0);
+            transform.Translate(0, ver * speed * Time.deltaTime * 4, 0);
         }
-
     }
 
     void FixedUpdate()
     {
         // controller.Move(horizontalMove * Time.fixedDeltaTime, false, false);
         //controller.Move(verticalMove * Time.fixedDeltaTime, false, false);
+    }
+
+    void Animate()
+    {
+        if (movementDirection != Vector2.zero)
+        {
+            animator.SetFloat("Horizontal", Input.GetAxis("Horizontal"));
+            animator.SetFloat("Vertical", Input.GetAxis("Vertical"));
+        }
+
+        animator.SetFloat("Speed", speed);
     }
 }
